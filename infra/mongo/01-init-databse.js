4df86c920bea56fd256d8055cad95e84aeea200e
HaIvaddd <halvaddd@gmail.com>
2026-02-01 21:21:07 +0300

    Mini fix

--- infra/mongo/01-init-databse.js
+++ infra/mongo/01-init-databse.js
@@ -1,8 +1,6 @@
-// Подключаемся под root
 db = db.getSiblingDB('admin');
 db.auth(process.env.MONGO_INITDB_ROOT_USERNAME, process.env.MONGO_INITDB_ROOT_PASSWORD);
 
-// Создаём БД notifications_db и пользователя
 db = db.getSiblingDB('notifications_db');
 db.createUser({
   user: 'notification_svc',
@@ -10,7 +8,6 @@ db.createUser({
   roles: [{ role: 'readWrite', db: 'notifications_db' }]
 });
 
-// Создаём БД configs_db и пользователя
 db = db.getSiblingDB('configs_db');
 db.createUser({
   user: 'config_svc',
